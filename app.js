a806391d980168743a922828b8cdc2f0c488a330
tf292201 <tf292201@gmail.com>
2024-02-08 18:46:13 -0500

    commit
    3 questions done

--- app.js
+++ app.js
@@ -1,49 +1,68 @@
-class Queue {
-  constructor() {
-    this.queue = [];
-  }
+//Challenges
+// 1. Browser
+let backStack = []; // Stack for back navigation
+let forwardStack = []; // Stack for forward navigation
 
-  enqueue(item) {
-    this.queue.push(item);
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return "Queue is empty";
-    }
-    return this.queue.shift();
-  }
-
-  peek() {
-    return this.queue[0];
-  } 
-
-  isEmpty() {
-    return this.queue.length === 0;
-  }
+function goToPage(url) {
+    backStack.push(url);
+    forwardStack = [];
+    navigate(url);
 }
 
-class Stack {
-  constructor() {
-    this.stack = [];
-  }
-
-  push(item) {
-    this.stack.push(item);
-  }
-
-  pop() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
+function goBack() {
+    if (backStack.length > 1) { 
+        let currentPage = backStack.pop();
+        forwardStack.push(currentPage);
+        navigate(backStack[backStack.length - 1]);
     }
-    return this.stack.pop();
-  }
-
-  peek() {
-    return this.stack[this.stack.length - 1];
-  }
-
-  isEmpty() {
-    return this.stack.length === 0;
-  }
 }
+function goForward() {
+    if (forwardStack.length > 0) { 
+        let nextPage = forwardStack.pop();
+        backStack.push(nextPage);
+        navigate(nextPage);
+    }
+}
+
+function navigate(url) {
+    console.log("Navigating to:", url);
+}
+// 2. Reversed String
+//defines new variable for reversed string, loops through the string and adds each character to the new variable
+function reverseString(str) {
+  let reversed = ''; 
+  for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str.charAt(i);
+  }
+  
+  return reversed; 
+}
+//3.brackets
+function isBalanced(str) {
+  const stack = []; 
+  
+  const bracketPairs = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+  };
+
+  for (let char of str) {
+    
+      if (bracketPairs[char]) {
+          stack.push(char);
+      }
+      // If the character is a closing bracket
+      else if (char === ')' || char === ']' || char === '}') {
+    
+          const lastBracket = stack.pop();
+         
+          if (bracketPairs[lastBracket] !== char) {
+              return false;
+          }
+      }
+  }
+
+  return stack.length === 0;
+}
+//4. Josephus Problem
